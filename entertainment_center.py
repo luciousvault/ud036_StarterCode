80022643a73e12124e007b89d7daa0bf4bbb92aa
Chris Williams <luciousvault12@gmail.com>
2017-10-12 18:58:40 -0400

    PEP8 check finds update

--- entertainment_center.py
+++ entertainment_center.py
@@ -6,106 +6,40 @@ import fresh_tomatoes
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toysthat come to life",
                         "https://upload.wikimedia.org/wikipedia/en/1/13/"
-                        + "Toy_Story.jpg",
+                        "Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=4KPTXpQehio")
 princessbride = media.Movie("Princess Bride",
-                     "A fairy tale adventure about a beautiful young woman "
-                        + "and her one true love. He must find her after "
-                        + "a long separation and save her. They must "
-                        + "battle the evils of the mythical kingdom "
-                        + "of Florin to be reunited with each other."
-                        + " Based on the William Goldman novel 'The "
-                        + "Princess Bride' which earned its own loyal "
-                        + "audience.",
-                     "http://quotesta.com/wp-content/uploads/"
-                        + "Princess-Bride-Poster-Original-1-1.jpg",
-                     "https://www.youtube.com/watch?v=VYgcrny2hRs")
+                            "An elderly man reads the book \"The Princess Bride\" to his sick and thus currently bedridden adolescent grandson, the reading of the book which has been passed down within the family for generations. The grandson is sure he won't like the story, with a romance at its core, he preferring something with lots of action and \"no kissing\". But the grandson is powerless to stop his grandfather, whose feelings he doesn't want to hurt. The story centers on Buttercup, a former farm girl who has been chosen as the princess bride to Prince Humperdinck of Florian. Buttercup does not love him, she who still laments the death of her one true love, Westley, five years ago. Westley was a hired hand on the farm, his stock answer of \"as you wish\" to any request she made of him which she came to understand was his way of saying that he loved her. But Westley went away to sea, only to be killed by the Dread Pirate Roberts. On a horse ride to clear her mind of her upcoming predicament of marriage, Buttercup is kidnapped by a band of bandits: Vizzini who works on his wits, and his two associates, a giant named Fezzik who works on his brawn, and a Spaniard named Inigo Montoya, who has trained himself his entire life to be an expert swordsman. They in turn are chased by the Dread Pirate Roberts himself. But chasing them all is the Prince, and his men led by Count Tyrone Rugen. What happens to these collectives is dependent partly on Buttercup, who does not want to marry the Prince, and may see other options as lesser evils, and partly on the other motives of individuals within the groups. But a larger question is what the grandson will think of the story as it proceeds and at its end, especially as he sees justice as high a priority as action.",  # noqa
+                            "http://quotesta.com/wp-content/uploads/Princess-Bride-Poster-Original-1-1.jpg",  # noqa
+                            "https://www.youtube.com/watch?v=VYgcrny2hRs")
 riddick = media.Movie("Riddick",
-                       "Riddick fights to survive and leave another "
-                       + "inhospitable planet",
-                       "http://cdn.collider.com/wp-content/uploads/"
-                       + "riddick-poster-3.jpg",
-                       "https://www.youtube.com/watch?v=ljKLtkSbIKY")
+                      "The infamous Riddick has been left for dead on a sun-scorched planet that appears to be lifeless. Soon, however, he finds himself fighting for survival against alien predators more lethal than any human he's encountered. The only way off is for Riddick to activate an emergency beacon and alert mercenaries who rapidly descend to the planet in search of their bounty. The first ship to arrive carries a new breed of merc, more lethal and violent, while the second is captained by a man whose pursuit of Riddick is more personal. With time running out and a storm on the horizon that no one could survive, his hunters won't leave the planet without Riddick's head as their trophy.",  # noqa
+                      "http://cdn.collider.com/wp-content/uploads/riddick-poster-3.jpg",  # noqa
+                      "https://www.youtube.com/watch?v=ljKLtkSbIKY")
 matrix = media.Movie("The Matrix",
-                     "Neo (Keanu Reeves) believes that Morpheus "
-                     + "(Laurence Fishburne), an elusive figure considered "
-                     + "to be the most dangerous man alive, can answer his "
-                     + "question -- What is the Matrix? Neo is contacted by"
-                     + " Trinity (Carrie-Anne Moss), a beautiful stranger "
-                     + "who leads him into an underworld where he meets "
-                     + "Morpheus. They fight a brutal battle for their lives"
-                     + " against a cadre of viciously intelligent secret "
-                     + "agents. It is a truth that could cost Neo something "
-                     + "more precious than his life.",
-                     "https://images-na.ssl-images-amazon.com/images/I/"
-                     + "51FhdGyJ6DL._SL500_AC_SS350_.jpg",
+                     "Thomas A. Anderson is a man living two lives. By day he is an average computer programmer and by night a hacker known as Neo. Neo has always questioned his reality, but the truth is far beyond his imagination. Neo finds himself targeted by the police when he is contacted by Morpheus, a legendary computer hacker branded a terrorist by the government. Morpheus awakens Neo to the real world, a ravaged wasteland where most of humanity have been captured by a race of machines that live off of the humans' body heat and electrochemical energy and who imprison their minds within an artificial reality known as the Matrix. As a rebel against the machines, Neo must return to the Matrix and confront the agents: super-powerful computer programs devoted to snuffing out Neo and the entire human rebellion.",  # noqa
+                     "https://images-na.ssl-images-amazon.com/images/I/51FhdGyJ6DL._SL500_AC_SS350_.jpg",  # noqa
                      "https://www.youtube.com/watch?v=m8e-FF8MsqU")
 # oldboy url might get taken down due to copyright.
 oldboy = media.Movie("Oldboy",
-                     "Dae-Su is an obnoxious drunk bailed from the police "
-                     + "station yet again by a friend. However, he's "
-                     + "abducted from the street and wakes up in a cell, "
-                     + "where he remains for the next 15 years, drugged "
-                     + "unconscious when human contact is unavoidable, "
-                     + "otherwise with only the television as company. And "
-                     + "then, suddenly released, he is invited to track "
-                     + "down his jailor with a denouement that is simply "
-                     + "stunning.",
-                     "http://img.moviepostershop.com/oldboy-movie-poster"
-                     + "-2003-1020263711.jpg",
+                     "Abducted on a rainy night in 1988, the obnoxious drunk, Oh Dae-Su, much to his surprise, wakes up locked in a windowless and dilapidated hotel room, for an unknown reason. There, his invisible and pitiless captors will feed him, clothe him and sedate him to avoid committing suicide, and as his only companion and a window to the world is the TV in his stark cell, the only thing that helps Oh Dae-Su keep going is his daily journal. But then, unexpectedly, after fifteen long years in captivity, the perplexed prisoner is deliberately released, encouraged to track down his tormentor to finally get his retribution. Nevertheless, who would hate Oh Dae-Su so much he would deny him of a quick and clean death?",  # noqa
+                     "http://img.moviepostershop.com/oldboy-movie-poster-2003-1020263711.jpg",  # noqa
                      "https://www.youtube.com/watch?v=D89OHw0VsYM")
 raid = media.Movie("The Raid Redemption",
-                   "A rookie member of an elite team of commandos,Rama" 
-                   + "(Iko Uwais) is instructed to hang back while his"
-                   + "comrades-in-arms go ahead with their mission to take "
-                   + "down a brutal crime lord called Tama (Ray Sahetapy). "
-                   + "However, the team's cover is blown, and Tama offers "
-                   + "sanctuary to every criminal in his high-rise apartment "
-                   + "block in exchange for the cops' heads. Now Rama must "
-                   + "take command and lead his remaining team on an "
-                   + "ultraviolent charge through the building to complete "
-                   + "-- and survive -- the mission.",
-                   "http://www.joblo.com/posters/images/full/raid-australian"
-                   + "-poster.jpg",
+                   "In Jakarta, Indonesia, Lieutenant Wahyu organizes the invasion of an apartment building that is the safe house of the powerful and cruel drug lord Tama and his gang. The SWAT team breaks in the building but one lookout sees and warns the gangsters and the police force is trapped on the seventh floor. They learn that Lt. Wahyu has not informed his superiors about the operation. Now the police officers have to fight with limited ammunition against the armed and dangerous gangsters.",  # noqa
+                   "http://www.joblo.com/posters/images/full/raid-australian-poster.jpg",  # noqa
                    "https://www.youtube.com/watch?v=m6Q7KnXpNOg")
 battle_royale = media.Movie("Battle Royale",
-                            "In the future, the Japanese government captures"
-                            + " a class of ninth-grade students and forces "
-                            + "them to kill each other under the "
-                            + "revolutionary \"Battle Royale\" act.",
-                            "https://resizing.flixster.com/boY6Rc6NtlDVQoQw"
-                            + "BzteIslnkVY=/206x305/v1.bTsxMTM3NjE3ODtqOzE3"
-                            + "NTAxOzEyMDA7OTAwOzEyMDA",
+                            "Forty-two students, three days, one deserted Island: welcome to Battle Royale. A group of ninth-grade students from a Japanese high school have been forced by legislation to compete in a Battle Royale. The students are each given a bag with a randomly selected weapon and a few rations of food and water and sent off to kill each other in a no-holds-barred (with a few minor rules) game to the death, which means that the students have three days to kill each other until one survives--or they all die. The movie focuses on a few of the students and how they cope. Some decide to play the game like the psychotic Kiriyama or the sexual Mitsuko, while others like the heroes of the movie--Shuya, Noriko, and Kawada--are trying to find a way to get off the Island without violence. However, as the numbers dwell down lower and lower on an hourly basis, is there any way for Shuya and his classmates to survive?",  # noqa
+                            "https://resizing.flixster.com/boY6Rc6NtlDVQoQwBzteIslnkVY=/206x305/v1.bTsxMTM3NjE3ODtqOzE3NTAxOzEyMDA7OTAwOzEyMDA",  # noqa
                             "https://www.youtube.com/watch?v=N0p1t-dC7Ko")
 kill_bill = media.Movie("Kill Bill Vol. 1",
-                        "The lead character, called 'The Bride,' was a member"
-                        + " of the Deadly Viper Assassination Squad, led by "
-                        + "her lover 'Bill.' Upon realizing she was pregnant "
-                        + "with Bill's child, 'The Bride' decided to escape "
-                        + "her life as a killer. She fled to Texas, met a young"
-                        + " man, who, on the day of their wedding rehearsal was"
-                        + " gunned down by an angry and jealous Bill (with the "
-                        + "assistance of the Deadly Viper Assassination Squad)."
-                        + " Four years later, 'The Bride' wakes from a coma, "
-                        + "and discovers her baby is gone. She, then, decides "
-                        + "to seek revenge upon the five people who destroyed "
-                        + "her life and killed her baby. The saga of Kill Bill "
-                        + "Volume I begins.",
-                        "http://s2.thingpic.com/images/xr/yJbD1JBhmpbD5AK2"
-                        + "mW6Aq95U.jpeg",
+                        "The lead character, called 'The Bride,' was a member of the Deadly Viper Assassination Squad, led by her lover 'Bill.' Upon realizing she was pregnant with Bill's child, 'The Bride' decided to escape her life as a killer. She fled to Texas, met a young man, who, on the day of their wedding rehearsal was gunned down by an angry and jealous Bill (with the assistance of the Deadly Viper Assassination Squad). Four years later, 'The Bride' wakes from a coma, and discovers her baby is gone. She, then, decides to seek revenge upon the five people who destroyed her life and killed her baby. The saga of Kill Bill Volume I begins.",  # noqa
+                        "http://s2.thingpic.com/images/xr/yJbD1JBhmpbD5AK2mW6Aq95U.jpeg",  # noqa
                         "https://www.youtube.com/watch?v=7kSuas6mRpk")
 boondock_saints = media.Movie("Boondock Saints",
-                              "Two Irish brothers accidentally kill mafia "
-                              + "thugs. They turn themselves in and are "
-                              + "released as heroes. They then see it as a "
-                              + "calling by God and start knocking off mafia"
-                              + " gang members one by one. Willem Dafoe plays"
-                              + " the detective trying to figure out the "
-                              + "killings, but the closer he comes to catching"
-                              + " the Irish brothers, the more he thinks the "
-                              + "brothers are doing the right thing.",
-                              "https://images-na.ssl-images-amazon.com/images"
-                              + "/I/51lgZ0YTcQL.jpg",
+                              "Two Irish brothers accidentally kill mafia thugs. They turn themselves in and are released as heroes. They then see it as a calling by God and start knocking off mafia gang members one by one. Willem Dafoe plays the detective trying to figure out the killings, but the closer he comes to catching the Irish brothers, the more he thinks the brothers are doing the right thing.",  # noqa
+                              "https://images-na.ssl-images-amazon.com/images/I/51lgZ0YTcQL.jpg",  # noqa
                               "https://www.youtube.com/watch?v=ydXojYfCF3I")
 
 
